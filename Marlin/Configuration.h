0131a22163e8fefba5ddfaceaecb9656392a9111
classicrocker883 <andrewleduc88@yahoo.com>
2023-03-31 15:47:04 -0400

    testest

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1126,7 +1126,7 @@
 #define V_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define W_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define Z_MIN_PROBE_ENDSTOP_INVERTING false // Set to true to invert the logic of the probe.
-
+//tests
 // Enable this feature if all enabled endstop pins are interrupt-capable.
 // This will remove the need to poll the interrupt pins, saving many CPU cycles.
 #define ENDSTOP_INTERRUPTS_FEATURE  // Ender Configs
