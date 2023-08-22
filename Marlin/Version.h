13e365df6916e508bdaa7f9c9e183161789c150d
classicrocker883 <andrewleduc88@yahoo.com>
2023-08-22 15:21:00 -0400

    Update 8/22 - Merge w/Marlin

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -41,10 +41,7 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-//#define STRING_DISTRIBUTION_DATE "2023-07-30"
-
-#define STRING_DISTRIBUTION_DATE __DATE__
-#define STRING_DISTRIBUTION_TIME __TIME__
+//#define STRING_DISTRIBUTION_DATE "2023-08-22"
 
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
